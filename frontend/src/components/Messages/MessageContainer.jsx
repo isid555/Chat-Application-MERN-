e1d8a1b2b3f5df58903fa4fd430d20d5253521d0
isid555 <r555sid@gmail.com>
2024-10-20 13:08:39 +0530

    Frontend-UI->Messages-2

--- frontend/src/components/Messages/MessageContainer.jsx
+++ frontend/src/components/Messages/MessageContainer.jsx
@@ -1,17 +1,43 @@
 import Messages from "./Messages.jsx";
+import MessageInput from "./MessageInput.jsx";
+import {TiMessage, TiMessages} from "react-icons/ti";
+
+
+
+
+const  NochatSelected = () =>{
+    return(
+        <div className={"flex items-center justify-center w-full h-full"}>
+        <div className={"px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2"}>
+            <p>Welcome John Doe</p>
+            <p>Select a chat to start messageing</p>
+            <TiMessages className={"text-3xl md:text-6xl text-center"}/>
+        </div>
+        </div>
+    )
+}
+
+
 
 const MessageContainer = ()=>{
+    const nochatSelected = true;
+
+
+
 return(
     <div className={"md:min-w-[450px] flex flex-col"}>
-    <>
-    {/*Header*/}
+        {nochatSelected ? <NochatSelected/> : (
+            <>
+                {/*Header*/}
 
-        <div className={"bg-slate-500 px-4 py-2 mb-2"}>
-        <span className={"label-text"}>To:</span> <span className={"text-gray-900"}>John Doe</span>
-        </div>
-        <Messages/>
-    {/*    Send message*/}
-    </>
+                <div className={"bg-slate-500 px-4 py-2 mb-2"}>
+                    <span className={"label-text"}>To:</span> <span className={"text-gray-900"}>John Doe</span>
+                </div>
+                <Messages/>
+                {/*    Send message*/}
+                <MessageInput/>
+            </>
+        )}
     </div>
 )
 }
