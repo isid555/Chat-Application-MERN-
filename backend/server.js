739cae09fde48845011841bafed51c9354bcd420
isid555 <r555sid@gmail.com>
2024-10-21 19:35:50 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -5,20 +5,23 @@ import authRoutes from "./routes/auth.routes.js";
 import connectToMDB from "./db/connectToMDB.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
-
+import path from "path"
 import {app,server,io} from "./socket/socket.js";
 app.use(express.json())
 app.use(cookieparser())
 dotenv.config()
 const PORT =process.env.PORT || 5000
-
+const __dirname = path.resolve()
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 app.use("/api/users",userRoutes)
-app.get("/" , (req,res)=>{
-    res.send("Hi therew")
-})
 
+app.use(express.static(path.join(__dirname,"frontend/dist")))
+
+
+app.get("*",(req,res) =>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 
 server.listen(PORT , ()=>{
     connectToMDB();
