66b72c425c26d7f53e6a55ce57117ea75572fe25
isid555 <r555sid@gmail.com>
2024-10-21 20:13:07 +0530

    Deployment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 import {BrowserRouter} from "react-router-dom";
 import {AuthContextProvider} from "./context/AuthContext.jsx";
 import {SocketContextProvider} from "./context/SocketContext.jsx";
+import './index.css'; // Adjust the path if necessary
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
